3b81a4d9b62d2f893761ce738d8622a77e03657f
MinhThanh <21521444@gm.uit.edu.vn>
2023-12-21 16:00:43 +0700

    Connect MongoDB in datasource

---
+++ e-commerce-platform/src/datasources/mongo.datasource.ts
@@ -0,0 +1,23 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'Mongo',
+  connector: 'mongodb',
+  url: 'mongodb+srv://alien:ED2bzNhvnlOG3N5x@alien.77u7gvp.mongodb.net/3-commerce',
+
+};
+
+@lifeCycleObserver('datasource')
+export class MongoDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'Mongo';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.Mongo', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
